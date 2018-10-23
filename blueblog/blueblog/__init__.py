d1c774c6263481351711bf3e9efa1fd8e501c6b0
authetic <1019892846@qq.com>
2018-10-23 22:28:53 +0800

    Add initial files

---
+++ blueblog/blueblog/__init__.py
@@ -0,0 +1,93 @@
+from flask import Flask, render_template
+from blueblog.settings import config
+from blueblog.extensions import bootstrap, db, moment, ckeditor, mail
+
+import os
+import click
+
+
+def create_app(config_name=None):
+
+    if config_name is None:
+        config_name = os.getenv('FLASK_CONFIG', 'development')
+
+    app = Flask('blueblog')
+    app.config.from_object(config[config_name])
+
+    register_logging(app)
+    register_extensions(app)
+    register_blueprints(app)
+    register_commands(app)
+    register_errors(app)
+    register_shell_context(app)
+    register_template_context(app)
+
+    return app
+
+def register_logging(app):
+    pass
+
+def register_extensions(app):
+    bootstrap.init_app(app)
+    db.init_app(app)
+    moment.init_app(app)
+    ckeditor.init_app(app)
+    mail.init_app(app)
+
+def register_blueprints(app):
+    pass
+
+def register_shell_context(app):
+
+    @app.shell_context_processor
+    def make_shell_context():
+        return dict(db=db)
+
+def register_template_context(app):
+    pass
+
+def register_errors(app):
+
+    @app.errorhandler(400)
+    def bad_request(e):
+        return render_template('errors/400.html')
+
+def register_commands(app):
+    @app.cli.command()
+    @click.option('--drop', is_flag=True, help='Create after drop')
+    def initdb(drop):
+        if drop:
+            click.confirm('This operation will delete the database, do you want to continue?', abort=True)
+            db.drop_all()
+            click.echo('Drop tables.')
+        db.create_all()
+        click.echo('Initialized database.')
+
+    @app.cli.command()
+    @click.option('--category', default=10, help='Quantity of categories, default is 10')
+    @click.option('--post', default=50, help='Quantity of posts, default is 50')
+    @click.option('--comment', default=500, help='Quantity of comments, default is 500')
+    def forge(category, post, comment):
+        from blueblog.fakes import fake_admin, fake_categories, fake_comments, fake_posts
+
+        db.drop_all()
+        db.create_all()
+
+        click.echo('Generating the administrator...')
+        fake_admin()
+
+        click.echo('Generating %d categories...' % category)
+        fake_categories(category)
+
+        click.echo('Generating %d posts...' % post)
+        fake_posts(post)
+
+        click.echo('Generating %d comments...' % comment)
+        fake_comments(comment)
+
+        click.echo('Done')
+
+    @app.cli.command()
+    def query():
+        from blueblog.models import Admin
+        print(Admin.query.filter_by(id=1))
