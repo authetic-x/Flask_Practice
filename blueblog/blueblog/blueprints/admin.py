5e552348e3fb6ff50bc3874fbef0ef0137b402c1
authetic <1019892846@qq.com>
2018-10-26 22:26:11 +0800

    Add edit_post

--- blueblog/blueblog/blueprints/admin.py
+++ blueblog/blueblog/blueprints/admin.py
@@ -4,6 +4,7 @@ from flask_login import login_required
 from blueblog.models import Post, Category
 from blueblog.forms import PostForm
 from blueblog.extensions import db
+from blueblog.utils import redirect_back
 
 
 admin_bp = Blueprint('admin', __name__)
@@ -13,7 +14,7 @@ admin_bp = Blueprint('admin', __name__)
 @login_required
 def manage_post():
     page = request.args.get('page', 1, type=int)
-    pagination = Post.query.order_by(Post.timestamp.desc()).paginage(
+    pagination = Post.query.order_by(Post.timestamp.desc()).paginate(
         page, per_page=current_app.config['BLUEBLOG_POST_PER_PAGE']
     )
     posts = pagination.items
@@ -35,10 +36,32 @@ def new_post():
         return redirect(url_for('.show_post', post_id=post.id))
     return render_template('admin/new_post.html', form=form)
 
-@admin_bp.route('/post/delete', methods=['POST'])
+@admin_bp.route('/post/<int:post_id>/edit', methods=['GET', 'POST'])
 @login_required
-def delete_post():
-    pass
+def edit_post(post_id):
+    form = PostForm()
+    post = Post.query.get_or_404(post_id)
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.body = form.body.data
+        post.category = Category.query.get(form.category.data)
+        db.session.add(post)
+        db.session.commit()
+        flash('Post updated', 'success')
+        return redirect(url_for('.show_post', post_id=post.id))
+    form.title.data = post.title
+    form.body.data = post.body
+    form.category.data = post.category_id
+    return render_template('admin/edit_post.html', form=form)
+
+@admin_bp.route('/post/<int:post_id>/delete', methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Post deleted', 'success')
+    return redirect_back()
 
 @admin_bp.route('/post/category', methods=['GET', 'POST'])
 @login_required
